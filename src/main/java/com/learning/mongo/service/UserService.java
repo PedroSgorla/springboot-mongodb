68a22649f79ff85a61bac28bc8acc64d4415ebca
Pedro Sgorla <pedro.oliveira@atlantecnologia.com.br>
2026-09-24 16:58:53 -0300

    implement findById for User and ResourceExceptionHandler

--- src/main/java/com/learning/mongo/service/UserService.java
+++ src/main/java/com/learning/mongo/service/UserService.java
@@ -1,12 +1,14 @@
 package com.learning.mongo.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.learning.mongo.domain.User;
 import com.learning.mongo.repository.UserRepository;
+import com.learning.mongo.service.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,5 +18,10 @@ public class UserService {
 	
 	public List<User> findAll(){
 		return repository.findAll();
+	}
+	
+	public User findById(String id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found!"));
 	}
 }
